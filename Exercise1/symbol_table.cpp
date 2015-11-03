518231cbfc7b02dba151621456481ae67f73487b
Yi Qin <yiqin.mems@gmail.com>
2015-11-02 19:00:11 -0600

    update ..

--- Exercise1/symbol_table.cpp
+++ Exercise1/symbol_table.cpp
@@ -1,14 +1,14 @@
 #include "symbol_table.h"
 
-void Symbol_Table::add (string key, int value) {
+void Symbol_Table::add (std::string key, int value) {
 	m[key] = value;
 }
 
-int Symbol_Table::get_value(string key) {
+int Symbol_Table::get_value(std::string key) {
 	return m[key];
 }
 
-bool Symbol_Table::is_variable_defined (string key) {
+bool Symbol_Table::is_variable_defined (std::string key) {
 	if (m.find(key) != m.end()) {
 		return true;
 	} else {
